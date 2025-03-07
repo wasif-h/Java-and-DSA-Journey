5070e32a4dd67f0d82f12cb381b1b2cae0c3e7c0
Wasif Hossain <wasif.hx@gmail.com>
2025-03-08 02:04:28 +0600

    update

--- Day 3/Patterns/basic.java
+++ Day 3/Patterns/basic.java
@@ -1,14 +1,12 @@
 import java.util.*;
+
 public class basic {
 
-    public static void p1()
-    {
+    public static void p1() {
         int n = 5;
 
-        for(int i = 1; i <= n ; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -17,14 +15,11 @@ public class basic {
         System.out.println("\n");
     }
 
-    public static void p2()
-    {
+    public static void p2() {
         int n = 5;
 
-        for(int i = 1; i <= n ; i++)
-        {
-            for(int j = 0; j < i; j++)
-            {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -33,15 +28,11 @@ public class basic {
         System.out.println();
     }
 
-
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         p1();
         p2();
-
-
 
     }
 }

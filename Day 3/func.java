20953f70079db5c5dbf36622add5585fdfed5892
Wasif Hossain <wasif.hx@gmail.com>
2025-03-07 15:02:41 +0600

    update

--- Day 3/func.java
+++ Day 3/func.java
@@ -1,3 +1,31 @@
+import java.util.*;
+
 public class func {
-    
+
+    // this is a method , bcz it placed inside a class
+    public static int sum(int a, int b, int c) {
+        int ans = a + b + c;
+        return ans;
+    }
+
+    public static void greet(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("Thank You-----");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter values a , b , c");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println("Total Sum = " + sum(a, b, c));
+        System.out.print("Greeting Amount : ");
+        int n = sc.nextInt();
+        greet(n);
+
+    }
 }
